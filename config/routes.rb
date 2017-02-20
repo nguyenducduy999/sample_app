ddaa72cd26c3abd728c819af02436470eb72a29b
nguyen duc duy <nguyen.duc.duy@framgia.com>
2017-02-20 16:48:08 +0700

    comme

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get "static_pages/home"
-
+  root "static_pages#home"
   get "static_pages/help"
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root "application#hello"
+  get  "static_pages/about"
 end
