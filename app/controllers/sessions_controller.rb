84b5b64797f0e91ff605f1c657f700e7d0831300
nguyen duc duy <nguyen.duc.duy@framgia.com>
2017-02-28 11:52:32 +0700

    cookie

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email].downcase
     if user && user.authenticate(params[:session][:password])
+      remember user
       log_in user
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = "Invalid email/password combination"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,41 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save :email_downcase
   has_secure_password
-
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
   validates :email, presence: true, length: {maximum: 255},
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
   validates :name, presence: true, length: {maximum: 50}
   validates :password, presence: true, length: {minimum: 6}
 
+  def authenticated?(remember_token)
+    return false unless remember_digest
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.degest(remember_token)
+  end
+
   private
   def email_downcase
     self.email = email.downcase
+  end
+
+  class << self
+    def degest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
 end
