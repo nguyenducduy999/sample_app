c160b604921f6a9fb732075a6ec1bea181d5a4d7
nguyen duc duy <nguyen.duc.duy@framgia.com>
2017-02-28 11:52:32 +0700

    delete

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get "sessions/new"
 
   root "static_pages#home"
-
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
@@ -10,5 +9,6 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+
   resources :users
   end
